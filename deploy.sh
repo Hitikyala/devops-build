2abae92a2736350fadd78e5c0823512b92c136ac
Hitikyala <hitikyala@gmail.com>
2024-08-21 21:34:06 +0000

    Add newly created files

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ docker stop dev || true
 docker rm dev || true
 
 # Push the image to the dev repository
-docker login -u hitikyala -p "$DOCKER_TOKEN"
+docker login -u hitikyala -p "$DOCKER_PASS"
 docker push hitikyala/dev:latest
 
 # Run the new container
-docker run -d -p 80:80 --name hitikyala/dev:latest
+docker run -d --name dev -p 80:80 hitikyala/dev:latest
